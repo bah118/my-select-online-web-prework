d9efcda8b70b3bb073e479ae0f265b1f196f5899
BA Hanlon <herbivore137@gmail.com>
2019-04-14 05:38:52 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,10 @@
 def my_select(collection)
  i = 0 
+ new = []
  while i < collection.length 
- yield collection[i]
+ if yield collection[i] == true 
+   new << collection[i]
  i += 1 
  end
- 
+ new
 end
